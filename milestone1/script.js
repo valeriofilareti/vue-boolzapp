6a1e8ea23a63e662976112b062516e5af40c82ef
valeriofilareti <valerio.filareti@gmail.com>
2023-04-18 11:49:23 +0200

    [add] chat section

--- milestone1/script.js
+++ milestone1/script.js
@@ -6,7 +6,7 @@ createApp({
       contacts: [
         {
           name: 'Michele',
-          avatar: 'img/avatar_1.png',
+          avatar: 'img/avatar_1.jpg',
           visible: true,
           messages: [
             {
